cb35a5a612b98a0f1329c123754d4009145f07c7
tylerlove <tml.335@gmail.com>
2018-05-17 00:39:15 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,7 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |combo|
+  WIN_COMBINATIONS.select do |combo|
     if(board[combo[0]] == 'X' && board[combo[1]] == 'X' && board[combo[2]] == 'X')
       return combo
     else
